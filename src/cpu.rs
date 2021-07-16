38429cf64f345dfa9793b5b71a2dffed322ee39f
arata <arata04962@gmail.com>
2021-07-17 01:16:58 +0900

    :sparkles: implement a memory

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,11 @@
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct CPU {
     pub register_a: u8,
     pub register_x: u8,
     pub status: u8,
-    pub program_counter: u8,
+    pub program_counter: u16,
+
+    memory: [u8; 0xffff],
 }
 
 pub const ZERO: u8 = 0b0000_0010;
@@ -11,19 +13,46 @@ pub const SIGN: u8 = 0b1000_0000;
 
 impl CPU {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            register_a: 0,
+            register_x: 0,
+            status: 0,
+            program_counter: 0,
+
+            memory: [0; 0xffff],
+        }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        self.program_counter = 0;
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
 
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.mem_write_u16(0xfffc, 0x8000);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+
+        self.program_counter = self.mem_read_u16(0xfffc);
+    }
+
+    pub fn run(&mut self) {
         loop {
-            let opcode = program[self.inc_pc()];
+            let opcode = self.mem_read(self.program_counter);
+            self.program_counter += 1;
 
             match opcode {
                 0x00 => return,
                 0xa9 => {
-                    let param = program[self.inc_pc()];
+                    let param = self.mem_read(self.program_counter);
+                    self.program_counter += 1;
+
                     self.lda(param);
                 }
                 0xaa => self.tax(),
@@ -65,10 +94,25 @@ impl CPU {
         self.status & flag != 0
     }
 
-    fn inc_pc(&mut self) -> usize {
-        let cur_pc = self.program_counter;
-        self.program_counter += 1;
-        cur_pc as usize
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_read_u16(&self, addr: u16) -> u16 {
+        let lo = self.mem_read(addr) as u16;
+        let hi = self.mem_read(addr + 1) as u16;
+        (hi << 8) | lo
+    }
+
+    fn mem_write_u16(&mut self, addr: u16, data: u16) {
+        let lo = (data & 0xff) as u8;
+        let hi = (data >> 8) as u8;
+        self.mem_write(addr, lo);
+        self.mem_write(addr + 1, hi);
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
     }
 }
 
@@ -79,7 +123,7 @@ mod test {
     #[test]
     fn test_0xa9_lda_immediate_load_data() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x05, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x05, 0x00]);
         assert_eq!(cpu.register_a, 0x05);
         assert!(!cpu.get_flag(ZERO));
         assert!(!cpu.get_flag(SIGN));
@@ -88,30 +132,28 @@ mod test {
     #[test]
     fn test_0xa9_lda_zero_flag() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0x00, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x00, 0x00]);
         assert!(cpu.get_flag(ZERO));
     }
 
     #[test]
     fn test_0xaa_tax_move_a_to_x() {
         let mut cpu = CPU::new();
-        cpu.register_a = 0x10;
-        cpu.interpret(vec![0xaa, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0x10, 0xaa, 0x00]);
         assert_eq!(cpu.register_x, 0x10);
     }
 
     #[test]
     fn test_5_ops_working_together() {
         let mut cpu = CPU::new();
-        cpu.interpret(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xc0, 0xaa, 0xe8, 0x00]);
         assert_eq!(cpu.register_x, 0xc1);
     }
 
     #[test]
     fn test_inx_overflow() {
         let mut cpu = CPU::new();
-        cpu.register_x = 0xff;
-        cpu.interpret(vec![0xe8, 0xe8, 0x00]);
+        cpu.load_and_run(vec![0xa9, 0xff, 0xaa, 0xe8, 0xe8, 0x00]);
         assert_eq!(cpu.register_x, 0x1);
     }
 }
